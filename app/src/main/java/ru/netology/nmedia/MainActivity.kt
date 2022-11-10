b82636e5b81c534640e036d438031bcc3f092ccb
Roman Leshin <vorzee@gmail.com>
2022-11-10 18:00:35 +0500

    Add red heart in resources

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageButton>(R.id.likes).setOnClickListener {
+            (it as ImageButton).setImageResource(R.drawable.ic_liked_24)
+        }
     }
 }
