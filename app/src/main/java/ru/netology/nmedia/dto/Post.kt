57883a9e6829904415925f1a50371257422eb299
Roman Leshin <104573735+vOrzee@users.noreply.github.com>
2022-12-30 12:36:25 +0500

    Homework 6. Reformat

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,5 +1,16 @@
 package ru.netology.nmedia.dto
 
+import android.util.JsonWriter
+import android.webkit.JsPromptResult
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import com.google.gson.JsonPrimitive
+import org.json.JSONArray
+import org.json.JSONObject
+
 data class Post(
     val id: Long,
     val author: String,
@@ -12,5 +23,17 @@ data class Post(
     val countShared: Int = 999,
     val viewedByMe: Boolean = false,
     val countViews: Int = 99_999,
-    val videoUrl: String? = null
+    val videoUrl: String? = null,
+    //val attachment: String? = null,
+
 )
+
+data class Attachment(
+    val url: String,
+    val description: String?,
+    val type: AttachmentType,
+)
+
+enum class AttachmentType {
+    IMAGE
+}
